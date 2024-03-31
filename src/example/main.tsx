3597889371b64f452d4ecb90ea0c8ca9be4cd09a
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-03-31 17:17:33 +0900

    feat: Add GitbeakerProvider to example app.

--- src/example/main.tsx
+++ src/example/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GitbeakerProvider } from "@/provider";
+
+const gitbeakerOptions = {
+  token: import.meta.env.VITE_GITLAB_TOKEN ?? "",
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <GitbeakerProvider options={gitbeakerOptions}>
+      <App />
+    </GitbeakerProvider>
   </React.StrictMode>,
 );
